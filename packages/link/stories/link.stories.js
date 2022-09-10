93271d182aa2d1aa9bcc4310e043e6787959ce5c
gaowenbin <lotosv2010@163.com>
2022-09-10 14:32:07 +0800

    Update link.stories.js

--- packages/link/stories/link.stories.js
+++ packages/link/stories/link.stories.js
@@ -1,7 +1,7 @@
 import GuiLink from '../src/link.vue'
 
 export default {
-  title: 'GuiLink',
+  title: '超链接',
   component: GuiLink
 }
 
@@ -9,7 +9,37 @@ export const Link = _ => ({
   components: { GuiLink },
   template: `
     <div>
+      <gui-link :disabled="false" href="http://www.baidu.com">baidu</gui-link>
+    </div>
+  `
+})
+
+Link.story = {
+  name: '基本'
+}
+
+export const LinkDisabled = _ => ({
+  components: { GuiLink },
+  template: `
+    <div>
       <gui-link :disabled="true" href="http://www.baidu.com">baidu</gui-link>
     </div>
   `
 })
+
+LinkDisabled.story = {
+  name: '禁用'
+}
+
+export const LinkUnderline = _ => ({
+  components: { GuiLink },
+  template: `
+    <div>
+      <gui-link :disabled="false" underline href="http://www.baidu.com">baidu</gui-link>
+    </div>
+  `
+})
+
+LinkUnderline.story = {
+  name: '下划线'
+}
